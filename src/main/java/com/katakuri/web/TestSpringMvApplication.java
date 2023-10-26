a2486cc97e75ae1b4b8d7cd92356bb3b709920f6
medoum <mohamedoum2430@gmail.com>
2023-10-26 11:43:20 +0100

    initial commit

---
+++ src/main/java/com/katakuri/web/TestSpringMvApplication.java
@@ -0,0 +1,13 @@
+package com.katakuri.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringMvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringMvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/katakuri/web/TestSpringMvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katakuri.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringMvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
